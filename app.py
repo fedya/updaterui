aba810963ce7571bd420a078e626abfe3b54dbf0
Alexander Stefanov <alexander@mezon.ru>
2023-02-25 00:47:46 +0000

    add github check, add url

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def run_command():
 def run_single():
     # execute the check_all.py script
     package = request.form['package']
-    subprocess.run(['/home/fdrt/webservice/6/generate.py --generate-single {}'.format(package)], shell=True)
+    subprocess.run(['./generate.py --generate-single {}'.format(package)], shell=True)
     # reload data from output.json file
     with open('output.json', 'r') as f:
         data = json.load(f)
@@ -41,7 +41,7 @@ def run_single():
 @app.route("/check_all")
 def check_all():
     # execute the check_all.py script
-    subprocess.run(['/home/fdrt/webservice/6/generate.py --generate-all'], shell=True)
+    subprocess.run(['./generate.py --generate-all'], shell=True)
     # reload data from output.json file
     with open('output.json', 'r') as f:
         data = json.load(f)
--- generate.py
+++ generate.py
@@ -20,6 +20,10 @@ headers = {
     "Cache-Control": "max-age=0",
 }
 
+api_key = os.environ.get('API_KEY')
+github_headers = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.2171.95 Safari/537.36',
+                  'Authorization': f'token {api_key}'}
+
 
 def check_python_module(package):
     url = "https://pypi.python.org/pypi/{}/json".format(package)
@@ -57,10 +61,58 @@ def compare_versions(v1, v2):
         pass
 
 
-def get_latest_version(package):
+def has_latin_letters(s):
+    pattern = re.compile(r'[a-zA-Z]')
+    return bool(pattern.search(s))
+
+def gh_check(package, url):
+    split_url = url.split("/")[:-1]
+    apibase = 'https://api.github.com/repos' + '/' + split_url[3] + '/' + split_url[4]
+    tag_url = apibase + "/tags"
+    release_url = apibase + "/releases/latest"
+    gh_versions = []
+
+    response_rel = requests.get(release_url, headers=github_headers)
+    print(release_url)
+    if response_rel.ok:
+       tag_name = response_rel.json()['tag_name']
+       if has_latin_letters(tag_name):
+           gh_versions.append("0")
+       else:
+           gh_versions.append(tag_name)
+
+    response_tag = requests.get(tag_url, headers=github_headers)
+    print(tag_url)
+    if response_tag.ok:
+       data = response_tag.json()
+       project_name = (data[0]['name'])
+       versions = sorted(data, key=lambda x: x['name'], reverse=True)
+       tag_version = versions[0]
+       print(tag_version)
+       clean_tag = tag_version["name"].strip("v")
+       if has_latin_letters(clean_tag):
+           gh_versions.append("0")
+       else:
+           gh_versions.append(clean_tag)
+
+    if len(gh_versions) > 1:
+       if compare_versions(gh_versions[0], gh_versions[1]) == "our-newer":
+           print("version obtained from TAG [{}] is newer than in RELEASE [{}] json".format(gh_versions[0], gh_versions[1]))
+           return gh_versions[0]
+       if compare_versions(gh_versions[0], gh_versions[1]) == "outdated":
+           print("version obtained from RELEASE [{}] is newer than in TAG [{}] json".format(gh_versions[1], gh_versions[0]))
+           return gh_versions[1]
+    if len(gh_versions) == 1:
+       return gh_versions[0]
+
+def get_latest_version(package, url_base):
     if package.startswith("python-"):
        package = package.split("-", 1)[1]
        return check_python_module(package)
+    if "github" in url_base:
+       print("checking github")
+       return gh_check(package, url_base)
+    return "0"
 
 
 def get_rosa_version(package):
@@ -81,7 +133,15 @@ def get_rosa_version(package):
             name = rpm.expandMacro("%{name}")
             version = rpm.expandMacro("%{version}")
             print("checking ROSA git repo: {}: {}".format(package, version))
-            return version
+            nvs = []
+            for (filename, num, flags) in rpm_spec.sources:
+                if num == 0 and flags == 1:
+                    # path
+                    # http://mirrors.n-ix.net/mariadb/mariadb-10.3.9/source/
+                    source_link = '/'.join(filename.split("/")[:-1])
+                    nvs.append(source_link)
+                    nvs.append(filename)
+                    return version, source_link
         except:
             return "broken"
         finally:
@@ -93,22 +153,24 @@ def update_single(package):
     with open('output.json', 'r') as f:
         data = json.load(f)
     found = False
-    rosa_version = get_rosa_version(package) # здесь нужно поставить функцию-заглушку для получения версии из Rosa
-    upstream_version = get_latest_version(package)
+    rosa_version, url_base = get_rosa_version(package) # здесь нужно поставить функцию-заглушку для получения версии из Rosa
+    upstream_version = get_latest_version(package, url_base)
+    status = compare_versions(rosa_version, upstream_version)
     for row in data:
         if row['package'] == package:
             row['version_rosa'] = rosa_version
             row['version_upstream'] = upstream_version
-            status = compare_versions(rosa_version, upstream_version)
             row['status'] = status
+            row['url'] = url_base
             found = True
             break
 
     if not found:
         new_row = {
             'package': package,
-            'version_rosa': get_rosa_version(package),
-            'version_upstream': get_latest_version(package),
+            'version_rosa': rosa_version,
+            'version_upstream': upstream_version,
+            'url': url_base,
             'status': '',
             'upgrade': ''
         }
@@ -123,8 +185,8 @@ def generate_json():
         packages = [line.strip() for line in f.readlines()]
     results = []
     for package in packages:
-        rosa_version = get_rosa_version(package) # здесь нужно поставить функцию-заглушку для получения версии из Rosa
-        upstream_version = get_latest_version(package)
+        rosa_version, url_base = get_rosa_version(package)
+        upstream_version = get_latest_version(package, url_base)
         if upstream_version is None:
             continue
         status = compare_versions(rosa_version, upstream_version)
